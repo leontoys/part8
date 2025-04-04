7ce454ec704a250ce8b9ab44f98b6e2c002e4a4c
leontoys <svliyon@gmail.com>
2025-04-05 07:41:21 +1100

    added websocket to frontend

--- library-frontend/src/main.jsx
+++ library-frontend/src/main.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { ApolloClient, InMemoryCache, gql, ApolloProvider, createHttpLink  } from "@apollo/client";
+import { ApolloClient, InMemoryCache, gql, ApolloProvider, createHttpLink,
+  split
+  } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
 
 const authLink = setContext((_, { headers }) => {  
   const token = localStorage.getItem('token')  
@@ -12,14 +17,33 @@ const authLink = setContext((_, { headers }) => {
       authorization: token ? `Bearer ${token}` : null,    
     }  }})
 
+
+    const wsLink = new GraphQLWsLink(createClient({
+      url: 'ws://localhost:4001',
+    }))
+    
+
     const httpLink = createHttpLink({
       uri: 'http://localhost:4001',
-    })    
+    }) 
+    
+    const splitLink = split(
+      ({ query }) => {
+        const definition = getMainDefinition(query)
+        return (
+          definition.kind === 'OperationDefinition' &&
+          definition.operation === 'subscription'
+        )
+      },
+      wsLink,
+      authLink.concat(httpLink)
+    )    
 
 //create client similar to how we created server in the gql backend
 const client = new ApolloClient({
   // uri: 'http://localhost:4001',
-  link: authLink.concat(httpLink),  
+  //link: authLink.concat(httpLink),  
+  link : splitLink,
   cache : new InMemoryCache()
 })
 
